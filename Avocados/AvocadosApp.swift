af7326676ba89a8a147b7e410acc87392d562e77
Miguel Jimenez <chamuel.developer@gmail.com>
2022-02-16 02:15:35 -0400

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by chamuel castillo on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
