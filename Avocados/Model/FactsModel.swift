abb3f4bbd9c845a2fe954067d260b570851e5847
Miguel Jimenez <chamuel.developer@gmail.com>
2022-02-27 16:27:10 -0400

    Create Facts feature

---
+++ Avocados/Model/FactsModel.swift
@@ -0,0 +1,18 @@
+//
+//  FactsModel.swift
+//  Avocados
+//
+//  Created by chamuel castillo on 2/26/22.
+//
+
+import SwiftUI
+
+//MARK: - FACT MODEL
+
+struct Fact: Identifiable {
+    var id = UUID()
+    var image: String
+    var content: String
+    
+    
+}
--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -8,15 +8,105 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: - PROPERTIES
+    
+    var headers: [Header] = headerData
+    var facts: [Fact] = factsData
+    var recipes: [Recipe] = recipesData
     var body: some View {
-        Text("Avocado Recipes")
-//        Image("background1")
-            .padding()
+        ScrollView(.vertical, showsIndicators:false){
+            VStack(alignment: .center, spacing: 20){
+                // MARK: - HEADER
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment:.top, spacing: 0) {
+                        ForEach(headers) { item in
+                            HeaderView(header: item)
+                        }
+                    }
+                }
+                
+                
+                //MARK: - DISHES
+                
+                Text("Avocado Dishes")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                DishesView()
+                    .frame(maxWidth:640 )
+                
+                //MARK: - FACTS
+                
+                Text("Avocados Facts")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                ScrollView(.horizontal,showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 60) {
+                        ForEach(facts) { item in
+                            FactsView(fact: item)
+                            
+                        }
+                    }
+                    .padding(.vertical)
+                    .padding(.leading, 60)
+                    .padding(.trailing, 20)
+                }
+        
+                
+                //MARK: - RECIPE CARDS
+                
+                Text("Avocado Recipes")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                VStack(alignment: .center, spacing: 20) {
+                    ForEach(recipes) { item in
+                        RecipeCardView(recipe: item)
+                    }
+                }
+                
+                //MARK: - FOOTER
+                
+                VStack(alignment: .center, spacing: 20) {
+                    Text("All About Avocados")
+                        .fontWeight(.bold)
+                        .modifier(TitleModifier())
+
+                    
+                    Text("Everything you wanted know about avocados but were too afraid to ask.")
+                        .font(.system(.body,design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.gray)
+                }
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+                
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
+    }
+}
+
+struct TitleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+        .font(.system(.title, design: .serif))
+        .foregroundColor(Color("ColorGreenAdaptive"))
+        .padding(8)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView(headers: headerData,facts: factsData,recipes: recipesData)
+                .previewLayout(.sizeThatFits)
+
+        }
+            
     }
 }
