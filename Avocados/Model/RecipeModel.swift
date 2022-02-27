fc95978b130ed1641645f9a749a4bb583658a993
Miguel Jimenez <chamuel.developer@gmail.com>
2022-02-27 16:28:04 -0400

    Add RecipeCardView

---
+++ Avocados/Model/RecipeModel.swift
@@ -0,0 +1,25 @@
+//
+//  RecipeModel.swift
+//  Avocados
+//
+//  Created by chamuel castillo on 2/26/22.
+//
+
+import SwiftUI
+
+//MARK: - RECIPE MODEL
+
+struct Recipe: Identifiable {
+    
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var rating: Int
+    var serves: Int
+    var preparation: Int
+    var cooking: Int
+    var instructions: [String]
+    var ingredients: [String]
+    
+}
